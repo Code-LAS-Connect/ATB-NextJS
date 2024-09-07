6e24fdcf582bd15ca15e35cd58553ca97f21adaf
LuxoJR-WD <diazcapadorw@hotmail.com>
2024-09-06 21:04:46 -0500

    Se hace merge con lara Sonic-v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['static.vecteezy.com', 'img.freepik.com', 'images.unsplash.com', 'your-image-domain.com'],
-      // Aca se agregan los Dominios de las imagenes 
-        },
-  };
+  images: {
+    domains: ['static.vecteezy.com', 'i.ytimg.com','img.freepik.com', 'images.unsplash.com', 'i.ytimg.com', 'upload.wikimedia.org', 'fincahotelarrayanes.com', 'conceptodefinicion.de', 'media-cdn.tripadvisor.com', 'images.ctfassets.net'],
+  },
+};
+
   
   export default nextConfig;
   
